216e04864c41a23f65f11541e0baab9bf9e611e5
Krist DevOps <87985901+Krist22@users.noreply.github.com>
2025-02-12 18:20:02 +0100

    Feat: Lancement du navigateur Firefox suite à la connexion VNC

--- startup.sh
+++ startup.sh
@@ -1,7 +1,19 @@
 #!/bin/sh
 
+# Démarrage de Xvfb
 Xvfb :1 -screen 0 1024x768x16 & 
 export DISPLAY=:1
+
+# Démarrage des services
 dbus-daemon --system & 
 startxfce4 & 
-x11vnc -display :1 -forever
+x11vnc -display :1 -forever &
+
+# Attente pour que l'environnement graphique soit prêt
+sleep ${BROWSER_WAIT}
+
+# Lancement du navigateur avec l'URL spécifiée
+/usr/bin/firefox --no-remote --new-window ${START_URL} &
+
+# Attente infinie pour maintenir le conteneur en vie
+wait
